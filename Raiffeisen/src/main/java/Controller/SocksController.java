736d45b7cd9dbe11dd638ee80197ebf760a404a5
Sсhelb <bokovikov.mikhail@gmail.com>
2021-10-29 22:28:34 +0400

    Add class Socks

--- Raiffeisen/src/main/java/Controller/SocksController.java
+++ Raiffeisen/src/main/java/Controller/SocksController.java
@@ -1,9 +1,40 @@
 package Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.print.DocFlavor;
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/sock")
 public class SocksController {
+    private List<String> socks;
+    @GetMapping("/api/socks")
+    public String getCountSocks(
+            @RequestParam(value = "color") String color,
+            @RequestParam(value= "operation") String operation,
+            @RequestParam(value= "cottonPart") String cottonPart ) {
+        return "getCountSocks";
+    }
+    @PutMapping("/api/socks/income")
+    public ResponseEntity<?> postIncomeSocks(
+            @RequestParam(value = "color") String color,
+            @RequestParam(value= "cottonPart") String cottonPart,
+            @RequestParam(value= "quantity") String quantity ) {
+        final boolean addSocks = socks.add(color);
+        return addSocks
+                ? new ResponseEntity<String>(HttpStatus.OK)
+                : new ResponseEntity<String>(HttpStatus.NOT_MODIFIED);
+    }
+    @PutMapping("/api/socks/outcome")
+    public ResponseEntity<?> postOutcomeSocks(
+            @RequestParam(value = "color") String color,
+            @RequestParam(value= "cottonPart") String cottonPart,
+            @RequestParam(value= "quantity") String quantity ) {
+        final boolean addSocks = socks.add(color);
+        return addSocks
+                ? new ResponseEntity<String>(HttpStatus.OK)
+                : new ResponseEntity<String>(HttpStatus.NOT_MODIFIED);
+    }
 }
--- Raiffeisen/src/main/java/com/example/Raiffeisen/RaiffeisenApplication.java
+++ Raiffeisen/src/main/java/com/example/Raiffeisen/RaiffeisenApplication.java
@@ -1,13 +1,14 @@
 package com.example.Raiffeisen;
 
+import Controller.SocksController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = SocksController.class)
 public class RaiffeisenApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RaiffeisenApplication.class, args);
 	}
-
 }
